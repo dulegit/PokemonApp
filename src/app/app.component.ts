9888e160bbaf847e9f9feac54d749302cd36c663
dulegit <dule.andrejevic@gmail.com>
2017-07-30 18:19:25 +0200

    First commit of Pokemon app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Pokémon App';
+  link = 'http://pokeapi.co/api/v2/pokemon/';
+  linkColor = 'http://pokeapi.co/api/v2/pokemon-color/';
+  http: Http;
+  pokemon = [];
+  pokemonColor = [];
+
+  constructor(http:Http) {
+  	this.http = http;
+  }
+
+  performSearch(searchTerm: HTMLInputElement): void {
+  	var apiLink = this.link + searchTerm.value;
+  	this.http.request(apiLink)
+  		.subscribe(
+  			(res: Response) => {
+  				this.pokemon = res.json();
+  				console.log(this.pokemon);
+  			},
+  			err => {
+  				this.pokemon = err;
+  				console.log(err);
+  			}
+  		);
+  	//console.log('User entered: ${searchTerm.value}');
+  	// bulbasaur
+  }
+  performSearchColor(searchColor: HTMLInputElement): void {
+  	var apiLink2 = this.linkColor + searchColor.value;
+  	this.http.request(apiLink2)
+  		.subscribe((res: Response) => {
+  			this.pokemonColor = res.json();
+  			console.log(this.pokemonColor);
+  		});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AlertModule } from 'ngx-bootstrap';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    AlertModule.forRoot(),
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
