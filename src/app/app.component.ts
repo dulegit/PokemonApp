c039d30b5c3f861af6e50cb26d9361b28e24508e
dulegit <dule.andrejevic@gmail.com>
2017-08-02 23:32:46 +0200

    Fixing errors on image src, catch err if not found

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Http, Response } from '@angular/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Pokémon App';
   link = 'http://pokeapi.co/api/v2/pokemon/';
@@ -13,6 +14,8 @@ export class AppComponent {
   http: Http;
   pokemon = [];
   pokemonColor = [];
+  termValue = "";
+  colorValue = "";
 
   constructor(http:Http) {
   	this.http = http;
@@ -24,22 +27,26 @@ export class AppComponent {
   		.subscribe(
   			(res: Response) => {
   				this.pokemon = res.json();
-  				console.log(this.pokemon);
+  				//console.log(this.pokemon['sprites'].front_default);
+          console.log(this.pokemon);
   			},
   			err => {
   				this.pokemon = err;
-  				console.log(err);
+          this.termValue = searchTerm.value;
   			}
-  		);
-  	//console.log('User entered: ${searchTerm.value}');
-  	// bulbasaur
+  		)
   }
+
   performSearchColor(searchColor: HTMLInputElement): void {
   	var apiLink2 = this.linkColor + searchColor.value;
   	this.http.request(apiLink2)
   		.subscribe((res: Response) => {
   			this.pokemonColor = res.json();
   			console.log(this.pokemonColor);
-  		});
+  		},
+      err => {
+        this.pokemonColor = err;
+        this.colorValue = searchColor.value;
+      })
   }
 }
